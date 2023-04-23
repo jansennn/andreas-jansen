eee947126db13e54bd4e89446329b916827dc931
Andreas Tampubolon <andreasjr61@gmail.com>
2023-04-23 19:00:28 +0700

    first commit

---
+++ src/data/socialLinks.js
@@ -0,0 +1,19 @@
+export const socialLinks = [{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: 'https://www.andreasjansen.me/',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/jansennn',
+	},
+	{
+		id: 3,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/andreas-tampubolon-595b8215a/',
+	},
+];
